bdfca62ed80ebca41222f3915f30d10aa4b1c2e4
irfan ullah <irfanullah7329@gmail.com>
2024-07-04 12:26:11 +0500

    initial commit

---
+++ home.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Dashboard - Home</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<script src="bootstrap/js/jquery.min.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="images/icon.svg" type="image/x-icon">
+    <link rel="stylesheet" href="css/sidenav.css">
+    <link rel="stylesheet" href="css/home.css">
+    <script src="js/restrict.js"></script>
+
+    <style>
+    body {font-family: Arial, Helvetica, sans-serif;}
+
+    #myImg {
+      border-radius: 5px;
+      cursor: pointer;
+      transition: 0.3s;
+    }
+
+    #myImg:hover {opacity: 0.7;}
+
+    /* The Modal (background) */
+    .modal {
+      display: none; /* Hidden by default */
+      position: fixed; /* Stay in place */
+      z-index: 1; /* Sit on top */
+      padding-top: 100px; /* Location of the box */
+      left: 0;
+      top: 0;
+      width: 100%; /* Full width */
+      height: 100%; /* Full height */
+      overflow: auto; /* Enable scroll if needed */
+      background-color: rgb(0,0,0); /* Fallback color */
+      background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+    }
+
+    /* Modal Content (image) */
+    .modal-content {
+      margin: auto;
+      display: block;
+      width: 80%;
+      max-width: 700px;
+    }
+
+    /* Caption of Modal Image */
+    #caption {
+      margin: auto;
+      display: block;
+      width: 80%;
+      max-width: 700px;
+      text-align: center;
+      color: #ccc;
+      padding: 10px 0;
+      height: 150px;
+    }
+
+    /* Add Animation */
+    .modal-content, #caption {
+      -webkit-animation-name: zoom;
+      -webkit-animation-duration: 0.6s;
+      animation-name: zoom;
+      animation-duration: 0.6s;
+    }
+
+    @-webkit-keyframes zoom {
+      from {-webkit-transform:scale(0)}
+      to {-webkit-transform:scale(1)}
+    }
+
+    @keyframes zoom {
+      from {transform:scale(0)}
+      to {transform:scale(1)}
+    }
+
+    /* The Close Button */
+    .close {
+      position: absolute;
+      top: 15px;
+      right: 35px;
+      color: #f1f1f1;
+      font-size: 40px;
+      font-weight: bold;
+      transition: 0.3s;
+    }
+
+    .close:hover,
+    .close:focus {
+      color: #bbb;
+      text-decoration: none;
+      cursor: pointer;
+    }
+
+    /* 100% Image Width on Smaller Screens */
+    @media only screen and (max-width: 700px){
+      .modal-content {
+        width: 100%;
+      }
+    }
+    </style>
+  </head>
+  <body>
+
+    <?php include "sections/sidenav.html"; ?>
+    <div class="container-fluid">
+      <div class="container">
+        <!-- header section -->
+        <?php
+          require "php/header.php";
+          createHeader('home', 'Dashboard', 'Home');
+        ?>
+        <!-- header section end -->
+        <img id="myImg"  src="images/largimage.jpg" alt="" style="width:100%;max-width:400px" hidden>
+
+        <!-- The Modal -->
+        <div id="myModal" class="modal" >
+          <span class="close">&times;</span>
+          <img class="modal-content" id="img01">
+          <div id="caption"></div>
+        </div>
+        <script>
+
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the image and insert it inside the modal - use its "alt" text as a caption
+        var img = document.getElementById("myImg");
+        var modalImg = document.getElementById("img01");
+        var captionText = document.getElementById("caption");
+        img.onload = function(){
+          modal.style.display = "block";
+          modalImg.src = this.src;
+          captionText.innerHTML = this.alt;
+        }
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+        </script>
+
+
+        <!-- form content -->
+        <div class="row">
+          <div class="row col col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+            <?php
+              function createSection1($location, $title, $table) {
+                require 'php/db_connection.php';
+
+                $query = "SELECT * FROM $table";
+                if($title == "Out of Stock")
+                  $query = "SELECT * FROM $table WHERE QUANTITY = 0";
+
+                $result = mysqli_query($con, $query);
+                $count = mysqli_num_rows($result);
+
+
+                if($title == "Expired") {
+                  // logic
+                  $count = 0;
+                  while($row = mysqli_fetch_array($result)) {
+                    $expiry_date = $row['EXPIRY_DATE'];
+                    if(substr($expiry_date, 3) < date('y'))
+                      $count++;
+                    else if(substr($expiry_date, 3) == date('y')) {
+                      if(substr($expiry_date, 0, 2) < date('m'))
+                        $count++;
+                    }
+                  }
+                }
+
+                echo '
+                  <div class="col-xs-12 col-sm-6 col-md-6 col-lg-4" style="padding: 10px">
+                    <div class="dashboard-stats" onclick="location.href=\''.$location.'\'">
+                      <a class="text-dark text-decoration-none" href="'.$location.'">
+                        <span class="h4">'.$count.'</span>
+                        <span class="h6"><i class="fa fa-play fa-rotate-270 text-warning"></i></span>
+                        <div class="small font-weight-bold">'.$title.'</div>
+                      </a>
+                    </div>
+                  </div>
+                ';
+              }
+              createSection1('manage_customer.php', 'Total Customer', 'customers');
+              createSection1('manage_supplier.php', 'Total Supplier', 'suppliers');
+              createSection1('manage_medicine.php', 'Total Medicine', 'medicines');
+              createSection1('manage_medicine_stock.php?out_of_stock', 'Out of Stock', 'medicines_stock');
+              createSection1('manage_medicine_stock.php?expired', 'Expired', 'medicines_stock');
+              createSection1('manage_invoice.php', 'Total Invoice', 'invoices');
+            ?>
+
+          </div>
+
+          <div class="col col-xs-4 col-sm-4 col-md-4 col-lg-4" style="padding: 7px 0; margin-left: 15px;">
+            <div class="todays-report">
+              <div class="h5">Todays Report</div>
+              <table class="table table-bordered table-striped table-hover">
+                <tbody>
+                  <?php
+                    require 'php/db_connection.php';
+                    if($con) {
+                      $date = date('Y-m-d');
+                  ?>
+                  <tr>
+                    <?php
+                      $total = 0;
+                      $query = "SELECT NET_TOTAL FROM invoices WHERE INVOICE_DATE = '$date'";
+                      $result = mysqli_query($con, $query);
+
+                      while($row = mysqli_fetch_array($result))
+                        $total = $total + $row['NET_TOTAL'];
+                    ?>
+                    <th>Total Sales</th>
+                    <th class="text-success">Rs. <?php echo $total; ?></th>
+                  </tr>
+                  <tr>
+                    <?php
+                      //echo $date;
+                      $total = 0;
+                      $query = "SELECT TOTAL_AMOUNT FROM purchases WHERE PURCHASE_DATE = '$date'";
+                      $result = mysqli_query($con, $query);
+                      while($row = mysqli_fetch_array($result))
+                        $total = $total + $row['TOTAL_AMOUNT'];
+                    }
+                    ?>
+                    <th>Total Purchase</th>
+                    <th class="text-danger">Rs. <?php echo $total; ?></th>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+        </div>
+
+        <hr style="border-top: 2px solid #ff5252;">
+
+        <div class="row">
+
+          <?php
+            function createSection2($icon, $location, $title) {
+              echo '
+                <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3" style="padding: 10px;">
+              		<div class="dashboard-stats" style="padding: 30px 15px;" onclick="location.href=\''.$location.'\'">
+              			<div class="text-center">
+                      <span class="h1"><i class="fa fa-'.$icon.' p-2"></i></span>
+              				<div class="h5">'.$title.'</div>
+              			</div>
+              		</div>
+                </div>
+              ';
+            }
+            createSection2('address-card', 'new_invoice.php', 'Create New Invoice');
+            createSection2('handshake', 'add_customer.php', 'Add New Customer');
+            createSection2('shopping-bag', 'add_medicine.php', 'Add New Medicine');
+            createSection2('group', 'add_supplier.php', 'Add New Supplier');
+            createSection2('bar-chart', 'add_purchase.php', 'Add New Purchase');
+            createSection2('book', 'sales_report.php', 'Sales Report');
+            createSection2('book', 'purchase_report.php', 'Purchase Report');
+          ?>
+
+        </div>
+        <!-- form content end -->
+
+        <hr style="border-top: 2px solid #ff5252;">
+
+      </div>
+    </div>
+  </body>
+</html>
+
+
+<script type="text/javascript">
+
+function imageload(){
+  $('.someImageElement').attr("src", 'someOriginalImageURL')
+
+  setTimeout(function(){
+       $('.someImageElement').attr("src", 'someNewImageURL');
+  }, 3000); // 3 seconds, adjust as needed
+}
+</script>
